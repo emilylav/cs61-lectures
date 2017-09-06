cb93f0f9b98bc2810e77534b093f5cb9a79b9fba
Eddie Kohler <ekohler@gmail.com>
2017-09-06 14:03:59 -0700

    Update after class.

--- l02/mexplore.c
+++ l02/mexplore.c
@@ -1,14 +1,22 @@
 #include <stdio.h>
 #include <stdlib.h>
+#include <assert.h>
 #include "hexdump.h"
+
+const int const_val = 10;
+int g = 0;
 
 void f(void) {
     char ch = 'A';
-    fprintf(stderr, "%c\n", ch);
+    int* p;
+    p = (int*) malloc(sizeof(int));
+    *p = 20000;
+    fprintf(stderr, "%c %d %d %p %d\n", ch, const_val, g, p, *p);
+    fprintf(stderr, "%zu %zu %zu %zu %zu\n", sizeof(ch), sizeof(const_val),
+            sizeof(g), sizeof(p), sizeof(*p));
+    fprintf(stderr, "%p %p %p %p %p\n", &ch, &const_val, &g, &p, &*p);
 }
 
-
-int main(int argc, char** argv) {
-    (void) argc, (void) argv;
+int main() {
     f();
 }
